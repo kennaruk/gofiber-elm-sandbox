b5a2a7b86133390cfefbd3ba4df9e22b02f62098
Ken Nitipat Wuttisasiwat <nitipat.wuttisasiwat@refinitiv.com>
2020-03-03 18:08:53 +0700

    add http and get response

---
+++ elm/src/Main.elm
@@ -0,0 +1,84 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, text, pre)
+import Http
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+  , Http.get
+      { url = "https://f6fe464c-0c51-4103-b906-25044eba435b.mock.pstmn.io/api"
+      , expect = Http.expectString GotText
+      }
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = GotText (Result Http.Error String)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    GotText result ->
+      case result of
+        Ok fullText ->
+          (Success fullText, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  case model of
+    Failure ->
+      text "I was unable to load your book."
+
+    Loading ->
+      text "Loading..."
+
+    Success fullText ->
+      pre [] [ text fullText ]
